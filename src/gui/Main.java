02832f9dfd8a2e648eb0ff00dba4172fcce6e8e2
Steven C <steven_c_ch@hotmail.com>
2014-05-30 19:08:04 +0200

    Added GUI to buy tickets, yet no functionality

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -3,7 +3,7 @@ package gui;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.fxml.FXMLLoader;
 
 
@@ -11,8 +11,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Design.fxml"));
-			Scene scene = new Scene(root,400,400);
+			FlowPane root = (FlowPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root,600,480);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
