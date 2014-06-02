35fc8c35dbd1ea241f57c0d361fd6d8deafce12f
Steven C <steven_c_ch@hotmail.com>
2014-06-02 20:39:04 +0200

    Added I18N support for GUI

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -3,10 +3,14 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 
-
-public class Controller {
+public class Controller implements Initializable {
 
     @FXML
     private ResourceBundle resources;
@@ -14,11 +18,40 @@ public class Controller {
     @FXML
     private URL location;
 
+    @FXML
+    private Label langEN;
+
+    @FXML
+    private ChoiceBox<String> listValidityDuration;
+
+    @FXML
+    private Label langDE;
+    
+    @FXML
+    private Label message;
+
+    public Controller () {
+    }
+    
+    @FXML
+    void langSwitchEnglish(ActionEvent event) {
+    	System.out.println("eng");
+    }
+
+    @FXML
+    void langSwitchGerman(ActionEvent event) {
+    	System.out.println("ger");
+    }
 
     @FXML
     void initialize() {
-
+        assert message != null : "fx:id=\"message\" was not injected: check your FXML file 'Main.fxml'.";
 
     }
 
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+        assert message != null : "fx:id=\"message\" was not injected: check your FXML file 'Main.fxml'.";
+		
+	}
 }
--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,5 +1,9 @@
 package gui;
 	
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import gui.Main;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -8,13 +12,17 @@ import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+	
+	private static Locale locale = new Locale("en","US");
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			FlowPane root = (FlowPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
-			Scene scene = new Scene(root,600,480);
+			FlowPane root = (FlowPane)FXMLLoader.load(getClass().getResource("Main.fxml"), ResourceBundle.getBundle("messages", locale));	
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("EuroMillions");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
